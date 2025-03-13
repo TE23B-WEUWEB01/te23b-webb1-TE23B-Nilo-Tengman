ac6d56fed92f7ad0b45925f1f9501eeb124bba5c
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2025-03-13 11:07:12 +0100

    tränat js, variabler och konatenering

---
+++ kapitel-8/java-intro/script.js
@@ -0,0 +1,14 @@
+
+// var ålder = 99;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+
+// alert (tal1 + "+" + tal2 + "=" + summa);
+
+var för = "karim";
+var eft = "ryde";
+var hel = för +" "+ eft;
+var hobby = "spel";
+var mening = "hej hej";
+alert(mening)
