a3d24c65dd749365fdbc4832a2493e4b5c22265d
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2025-03-21 14:06:49 +0100

    jv script sidbyggande

---
+++ kapitel-8/events-js/script.js
@@ -0,0 +1,19 @@
+// while (true) {
+//     let namn = promt("Ange ditt namn:")
+//     let lösen = promt("Ange ditt lösenord:")
+
+//     if (namn == "alex" && lösen == "12345"){
+//         document.writeln("!!!yay1!!<br>");
+//         break;
+//     }
+//     else {
+//         document.writeln("fuck u<br>");
+//     }
+// }
+// let ålder = prompt("hur gammal är du?");
+// document.writeln(`Hej Alex du är ${ålder}`);
+
+// let tal = parseInt(prompt("nummer mellan 1-5?"));
+// for (let i = 0; i < tal; i++) {
+//     document.writeln("fuck u<br>");
+// }
