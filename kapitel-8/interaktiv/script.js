335ce2d172ba80e109eea67ba0783dfc17fce380
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2025-03-27 11:08:29 +0100

    tränat på onclick, functioner, queryselector

---
+++ kapitel-8/interaktiv/script.js
@@ -0,0 +1,17 @@
+
+function Dark() {
+    document.body.style = "background: #000; color: #FFF;";
+    document.querySelector("h1").style = "font-size: 100px;"
+}
+function Light() {
+    document.body.style = "background: #FFF; color: #000;";
+}
+function Konstig() {
+    document.querySelector("h1").style = "font-size: 70px; text-shadow: 5px 5px 5px pink;";
+}
+function Bla() {
+    document.querySelector("p").style = "visibility: visible;";
+}
+function InteBla() {
+    document.querySelector("p").style = "visibility: invisible;";
+}
